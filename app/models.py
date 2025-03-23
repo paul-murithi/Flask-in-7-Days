2f3b75a50f8bc4836028cf2cb966024d62c571bb
paul-murithi <paulmurithi233@gmail.com>
2025-03-23 20:23:25 +0300

    Database relationships

--- app/models.py
+++ app/models.py
@@ -1,4 +1,5 @@
 from flask_sqlalchemy import SQLAlchemy
+from datetime import datetime, timezone
 
 db = SQLAlchemy()
 
@@ -6,6 +7,40 @@ class User(db.Model):
     id = db.Column(db.Integer, primary_key=True)
     username = db.Column(db.String(80), unique=True, nullable=False)
     email = db.Column(db.String(120), unique=True, nullable=False)
+    created_at = db.Column(db.DateTime, default=datetime.now(timezone.utc))
 
     def __repr__(self):
         return f"<User {self.username}>"
+    
+# Database relationships
+# 1. One-to-Many relationship A Student can have multiple hobbies
+class Student(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String(255), nullable=False)
+    hobbies = db.relationship('Hobby', backref='owner', lazy=True)
+    
+class Hobby(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String(255), nullable=False)
+    student_id = db.Column(db.Integer, db.ForeignKey('student.id'), nullable=False)
+
+class Post(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    # Post can  have many comments
+    comments = db.relationship('Comment', backref="post", lazy=True)
+
+class Comment(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    content = db.Column(db.Text, nullable=False)
+    
+    post_id = db.Column(db.Integer, db.ForeignKey('post.id'), nullable=False)
+
+
+
+
+
+"""
+One post can have many comments  : One to Many relationship
+Post - The parent table
+Comment - The child table
+"""
