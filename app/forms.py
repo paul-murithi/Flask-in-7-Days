8eab144ebf0339304d9b3cdb44c33f5ed867bdd7
paul-murithi <paulmurithi233@gmail.com>
2025-04-01 08:52:09 +0300

    new file structure

--- app/forms.py
+++
@@ -1,7 +0,0 @@
-from flask_wtf import FlaskForm
-from wtforms import StringField, SubmitField
-from wtforms.validators import DataRequired
-
-class SearchForm(FlaskForm):
-    query = StringField('Search Query', validators=[DataRequired()])
-    submit = SubmitField('Search')
