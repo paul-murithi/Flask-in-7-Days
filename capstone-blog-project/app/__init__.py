2a2a132ae12fcdcd03a009d6103ef73420a4e705
paul-murithi <paulmurithi233@gmail.com>
2025-04-03 20:28:45 +0300

    config setup

--- capstone-blog-project/app/__init__.py
+++ capstone-blog-project/app/__init__.py
@@ -1,8 +1,8 @@
 from flask import Flask
-from .config import Config
-from .extensions import db, login_manager
-from .blueprints.auth import auth_bp
-from .blueprints.posts import posts_bp
+from .config import config
+# from .extensions import db, login_manager
+# from .blueprints.auth import auth_bp
+# from .blueprints.posts import posts_bp
 
 # The app factory function that creates the Flask app
 def create_app():
@@ -10,15 +10,15 @@ def create_app():
     app = Flask(__name__)
 
     # Load configurations from the Config class
-    app.config.from_object(Config)  # Config class (can be Dev or Prod)
+    app.config.from_object(config)  # Config class (can be Dev or Prod)
 
     # Initialize extensions like the database and login manager
-    db.init_app(app)
-    login_manager.init_app(app)
+    # db.init_app(app)
+    # login_manager.init_app(app)
 
     # Register blueprints (modularize the app)
-    app.register_blueprint(auth_bp)
-    app.register_blueprint(posts_bp)
+    # app.register_blueprint(auth_bp)
+    # app.register_blueprint(posts_bp)
 
     # Return the app instance
     return app
